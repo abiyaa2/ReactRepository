829af6243e86b045c546aa1856233075eb8ea0bd
this-is-karthikking <karthikraja2603@gmail.com>
2024-10-02 11:23:54 +0530

    CGT-17  Decrement should not allow negative integers

--- src/REDUX/ReduxCounter/ReduxCounter.jsx
+++ src/REDUX/ReduxCounter/ReduxCounter.jsx
@@ -1,36 +1,38 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, reset } from "../ACTIONS/CountAction";
 import "../ReduxCounter/button.css";
 
-const ReduxCounter=()=>
-{
- const count = useSelector((state)=>state.counter);
- const dispatch = useDispatch();
+const ReduxCounter = () => {
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
-    const incrementApp=()=>
-    {
-       dispatch(increment(1));
+  const incrementApp = () => {
+    dispatch(increment(1));
+  };
+  const decrementApp = () => {
+    if (count > 0) {
+      dispatch(decrement(1));
     }
-    const decrementApp=()=>
-    {
-        dispatch(decrement(1));
-    }
-    const resetApp=()=>
-    {
-       dispatch(reset());
-    }
-    return(
-        <>
-        <h4 className="px-2"> Counter : {count}</h4>
-        <div className="counter-button">
-       
-        <button className="btn btn-success me-3" onClick={incrementApp}> Increment</button>
-        <button className=" btn btn-danger me-3" onClick={decrementApp}>Decrement</button>
-        <button className="btn btn-primary me-3" onClick={resetApp}>Reset</button>
-        </div>
-       
-        
-        </>
-    )
-}
+  };
+  const resetApp = () => {
+    dispatch(reset());
+  };
+  return (
+    <>
+      <h4 className="px-2"> Counter : {count}</h4>
+      <div className="counter-button">
+        <button className="btn btn-success me-3" onClick={incrementApp}>
+          {" "}
+          Increment
+        </button>
+        <button className=" btn btn-danger me-3" onClick={decrementApp}>
+          Decrement
+        </button>
+        <button className="btn btn-primary me-3" onClick={resetApp}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
+};
 export default ReduxCounter;
